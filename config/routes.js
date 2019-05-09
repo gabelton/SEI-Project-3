0717405f7e4be63a13ee2b64a8141d0c2d3dbb24
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-09 15:42:00 +0100

    Added secureRoute

--- config/routes.js
+++ config/routes.js
@@ -1,17 +1,18 @@
 const router = require('express').Router()
+const secureRoute = require('../lib/secureRoute')
 const vinylsController = require('../controllers/vinyls')
 const authController = require('../controllers/auth')
 
 router.get('/', (req, res) => res.json({ message: 'Welcome to Project Black' }))
 
 router.get('/vinyls', vinylsController.index)
-router.post('/vinyls', vinylsController.create)
+router.post('/vinyls', secureRoute, vinylsController.create)
 router.get('/vinyls/:id', vinylsController.show)
-router.put('/vinyls/:id', vinylsController.update)
-router.delete('/vinyls/:id', vinylsController.delete)
+router.put('/vinyls/:id', secureRoute, vinylsController.update)
+router.delete('/vinyls/:id', secureRoute, vinylsController.delete)
 
 router.post('/register', authController.register)
-router.post('/login', authController.login) 
+router.post('/login', authController.login)
 
 
 module.exports = router
