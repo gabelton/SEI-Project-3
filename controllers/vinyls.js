fd54e9580b9b05860397c66cc3dcfb67fd62ce15
Emma Price <ekprice01@gmail.com>
2019-05-09 14:37:20 +0100

    Added showroute and createroute

--- controllers/vinyls.js
+++ controllers/vinyls.js
@@ -5,6 +5,18 @@ function indexRoute(req, res) {
     .then(vinyls => res.json(vinyls))
 }
 
+function showRoute(req, res) {
+  Vinyl.findById(req.params.id)
+    .then(vinyl => res.json(vinyl))
+}
+
+function createRoute(req, res) {
+  Vinyl.create(req.body)
+    .then(vinyl => res.status(201).json(vinyl))
+}
+
 module.exports = {
-  index: indexRoute
+  index: indexRoute,
+  show: showRoute,
+  create: createRoute 
 }
