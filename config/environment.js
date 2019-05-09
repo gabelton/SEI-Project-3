34275d7c7078a03592f667f0ebf1fab035ca872d
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-09 14:49:14 +0100

    added other routes

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/database-name'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/project-black'
 
 module.exports = { port, dbURI }
--- config/routes.js
+++ config/routes.js
@@ -5,13 +5,13 @@ const authController = require('../controllers/auth')
 router.get('/', (req, res) => res.json({ message: 'Welcome to Project Black' }))
 
 router.get('/vinyls', vinylsController.index)
-/*router.post('/vinyls', vinylsController.create)
+router.post('/vinyls', vinylsController.create)
 router.get('/vinyls/:id', vinylsController.show)
 router.put('/vinyls/:id', vinylsController.update)
 router.delete('/vinyls/:id', vinylsController.delete)
 
 router.post('/register', authController.register)
-router.post('/login', authController.login) */
+router.post('/login', authController.login) 
 
 
 module.exports = router
