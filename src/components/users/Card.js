fe422b2d58d256422041151b4eb39e36ef8c9036
Sean Gray <sean.myles.gray@gmail.com>
2019-05-10 15:49:33 +0100

    adding some styling

--- src/components/users/Card.js
+++ src/components/users/Card.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Card = ({ image, username, bio }) => {
   return(
     <section className="section">
-      <div className="container">
+      <div className="container tag:red">
         <div className="columns">
 
-          <div className="column is-third">
+          <div className="column is-third tag:blue">
             <div className="user-info">
               <div className="user-image">
                 <figure className="image">
@@ -14,26 +14,26 @@ const Card = ({ image, username, bio }) => {
                 </figure>
               </div>
               <div className="username">
-                <h3 className="username-title">{username}</h3>
+                <h3 className="profile">{username}</h3>
               </div>
               <div className="user-bio">
-                <p>{bio}</p>
+                <p className="profile">{bio}</p>
               </div>
             </div>
           </div>
 
           <div className="column is-third">
             <div className="placeholder1">
-              <h3 className="subtitle">Vinyl collection</h3>
+              <h3 className="subtitle profile">Vinyl collection</h3>
             </div>
           </div>
           <div className="column is-third">
             <div className="placeholder2">
               <div className="wishlist">
-                <h3 className="subtitle">wishlist</h3>
+                <h3 className="subtitle profile">wishlist</h3>
               </div>
               <div className="selllist">
-                <h3 className="subtitle">selllist</h3>
+                <h3 className="subtitle profile">selllist</h3>
               </div>
             </div>
           </div>
