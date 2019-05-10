2c030af0edcfdc803a5f3c7c211381878ae83ee5
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-10 10:42:59 +0100

    Added users controller and user routes

---
+++ controllers/users.js
@@ -0,0 +1,40 @@
+const User = require('../models/User')
+
+function indexRoute(req, res) {
+  User.find()
+    .then(users => res.json(users))
+}
+
+function showRoute(req, res) {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
+}
+
+function createRoute(req, res) {
+  User.create(req.body)
+    .then(user => res.status(201).json(user))
+}
+
+function updateRoute(req, res) {
+  User.findById(req.params.id)
+    .then(user => user.set(req.body))
+    .then(user => user.save())
+    .then(user => res.json(user))
+}
+
+function deleteRoute(req, res) {
+  User.findById(req.params.id)
+    .then(user => user.remove())
+    .then(() => res.sendStatus(204))
+}
+
+
+
+
+module.exports = {
+  index: indexRoute,
+  show: showRoute,
+  create: createRoute,
+  update: updateRoute,
+  delete: deleteRoute
+}
