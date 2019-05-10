2faf70826702ffcae9dfa206fb3fab0fb23a2981
Emma Price <ekprice01@gmail.com>
2019-05-10 12:28:27 +0100

    updated vinyl show

--- src/components/vinyls/Show.js
+++ src/components/vinyls/Show.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
+import Promise from 'bluebird'
 import Auth from '../../lib/Auth'
 
 class Show extends React.Component {
@@ -20,46 +21,54 @@ class Show extends React.Component {
 
   render() {
     if(!this.state.vinyl) return null
-    const { _id, artist, title, image, releaseYear, notes, genre, condition, length, label, size, format, speed, catalogueNumber, barcode } = this.state.vinyl
+    const { _id, artist, title, image, releaseYear, notes, genre, condition, length, label, size, format, speed, catalogueNumber, barcode, createdBy } = this.state.vinyl
 
     return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title is-1">{artist}: {title}</h1>
-          <hr />
-          <div className="columns">
-            <div className="column is-one-third-desktop is-full-tablet is-full-mobile">
-              <figure className="image">
-                <img src={image} alt={title} />
-              </figure>
-            </div>
-            <div className="column is-two-thirds-desktop is-full-tablet is-full-mobile">
-              <h2 className="subtitle is-5">Artist: {artist}</h2>
-              <h2 className="subtitle is-5">Title: {title}</h2>
-              <h2 className="subtitle is-5">Label: {label}</h2>
-              <h2 className="subtitle is-5">Year released: {releaseYear}</h2>
-              <h2 className="subtitle is-5">Genre: {genre}</h2>
-              <h2 className="subtitle is-5">Length: {length}</h2>
-              <h2 className="subtitle is-5">Notes: {notes}</h2>
-              <h2 className="subtitle is-5">Condition: {condition}</h2>
-              <h2 className="subtitle is-5">Size: {size}</h2>
-              <h2 className="subtitle is-5">Format: {format}</h2>
-              <h2 className="subtitle is-5">Speed: {speed}</h2>
-              <h2 className="subtitle is-5">Barcode: {barcode}</h2>
-              <h2 className="subtitle is-5">Catalogue number: {catalogueNumber}</h2>
+      <body>
+        <section className="section">
+          <div className="container">
+            <h1 className="title is-1">{artist}: {title}</h1>
+            <hr />
+            <div className="columns">
+              <div className="column is-one-third-desktop is-half-tablet is-full-mobile">
+                <figure className="image">
+                  <img src={image} alt={title} />
+                </figure>
+              </div>
+              <div className="column is-two-thirds-desktop is-half-tablet is-full-mobile">
+                <h2 className="subtitle is-5">Artist: {artist}</h2>
+                <h2 className="subtitle is-5">Title: {title}</h2>
+                <h2 className="subtitle is-5">Label: {label}</h2>
+                <h2 className="subtitle is-5">Year released: {releaseYear}</h2>
+                <h2 className="subtitle is-5">Genre: {genre}</h2>
+                <h2 className="subtitle is-5">Length: {length}</h2>
+                <h2 className="subtitle is-5">Notes: {notes}</h2>
+                <h2 className="subtitle is-5">Created by: {createdBy}</h2>
+              </div>
             </div>
           </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <div className="columns">
+              <div className="column is-one-third-desktop is-half-tablet is-full-mobile">
+                <h2 className="subtitle is-5">Condition: {condition}</h2>
+                <h2 className="subtitle is-5">Size: {size}</h2>
+                <h2 className="subtitle is-5">Format: {format}</h2>
+                <h2 className="subtitle is-5">Speed: {speed}</h2>
+                <h2 className="subtitle is-5">Barcode: {barcode}</h2>
+                <h2 className="subtitle is-5">Catalogue number: {catalogueNumber}</h2>
+                <h2 className="subtitle is-5">Tracklisting:</h2>
+              </div>
 
-          <div className="columns">
-            <div className="column is-half-desktop is-full-tablet is-full-mobile">
-            </div>
+              <div className="column is-two-thirds-desktop is-half-tablet is-full-mobile">
 
-            <div className="column is-half-desktop is-full-tablet is-full-mobile">
 
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </body>
     )
   }
 }
