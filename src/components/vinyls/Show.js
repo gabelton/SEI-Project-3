acad2507e15102109024eb017e591677ced6fc87
Gabe Naughton <gabriel.naughton135@gmail.com>
2019-05-15 11:54:04 +0100

    Added refresh to click events

--- src/components/vinyls/Show.js
+++ src/components/vinyls/Show.js
@@ -66,13 +66,16 @@ class Show extends React.Component {
   }
 
   handleClick(e) {
-    e.preventDefault()
+    //e.preventDefault()
+
 
     const token = Auth.getToken()
 
     axios.post(`/api/vinyls/${this.props.match.params.id}/comments`, this.state.data, {
       headers: { 'Authorization': `Bearer ${token}` }
     })
+
+    window.location.reload()
   }
 
 
@@ -90,6 +93,7 @@ class Show extends React.Component {
         headers: { 'Authorization': `Bearer ${token}` }
       } )
     }
+    window.location.reload()
   }
 
   handleDelete() {
