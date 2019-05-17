d22c08002461b9ef3514e7da1a731928f238427e
Dragan Ceran <dragandceran@gmail.com>
2019-05-17 09:46:07 +0100

    Team page

--- src/components/common/Team.js
+++ src/components/common/Team.js
@@ -3,106 +3,91 @@ import React from 'react'
 const Team = () => {
   return (
     <div className="container teamContainer">
-    <div className=" section columns  teambox">
-      <div className="column">
-        <div className="box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-128x128">
-                <img  className="is-rounded" src="https://i.imgur.com/fZXBxMO.jpg" alt="Image"/>
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>Emma Price</strong> <small>@johnsmith</small> <small>31m</small>
-                  <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-                </p>
+      <div className=" section columns  teambox">
+        <div className="column">
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-128x128">
+                  <img  className="is-rounded" src="https://i.imgur.com/fZXBxMO.jpg" alt="Image"/>
+                </figure>
               </div>
-
-            </div>
-          </article>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Emma Price</strong> <small>@johnsmith</small>
+                    <br />
+                        API ninja
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="column">
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-128x128">
+                  <img className="is-rounded" src="https://i.imgur.com/Nj2rE9g.jpg" alt="Image"/>
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Gabe Naughton</strong> <small>@johnsmith</small>
+                    <br />
+                      GIT master
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
         </div>
       </div>
-      <div className="column">
-      <div className="box">
-        <article className="media">
-          <div className="media-left">
-            <figure className="image is-128x128">
-              <img className="is-rounded" src="https://i.imgur.com/Nj2rE9g.jpg" alt="Image"/>
-            </figure>
+      <div className=" section columns  teambox">
+        <div className="column">
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-128x128">
+                  <img  className="is-rounded" src="https://scontent-lhr3-1.xx.fbcdn.net/v/t1.0-9/13102893_10206649682616828_2040755755404317176_n.jpg?_nc_cat=111&_nc_ht=scontent-lhr3-1.xx&oh=72b59d3ee3a0f57035f99b72ed138fea&oe=5D6719B1" alt="Image"/>
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Dragan Ceran</strong> <small>dragandceran@gmail.com</small>
+                    <br />
+                    Style nerd
+                  </p>
+                </div>
+              </div>
+            </article>
           </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>Gabe Naughton</strong> <small>@johnsmith</small> <small>31m</small>
-                <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-              </p>
-            </div>
-
+        </div>
+        <div className="column">
+          <div className="box">
+            <article className="media">
+              <div className="media-left">
+                <figure className="image is-128x128">
+                  <img className="is-rounded" src="https://i.imgur.com/OgeplkZ.jpg" alt="Image"/>
+                </figure>
+              </div>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Sean Gray</strong> <small>sean.myles.gray@gmail.com</small>
+                    <br />
+                      .MAP sorcerer
+                  </p>
+                </div>
+              </div>
+            </article>
           </div>
-        </article>
+        </div>
       </div>
     </div>
-</div>
-<div className=" section columns  teambox">
-
-      <div className="column">
-      <div className="box">
-        <article className="media">
-          <div className="media-left">
-          <figure className="image is-128x128">
-  <img className="is-rounded" src="https://i.imgur.com/OgeplkZ.jpg" alt="Image"/>
-            </figure>
-          </div>
-          <div className="media-content">
-            <div className="content">
-              <p>
-                <strong>Sean Gray</strong> <small>sean.myles.gray@gmail.com</small> <small>31m</small>
-                <br />
-
-              </p>
-            </div>
-
-          </div>
-        </article>
-      </div>
-    </div>
-
-
-
-
-
-    <div className="column">
-    <div className="box">
-      <article className="media">
-        <div className="media-left">
-        <figure className="image is-128x128">
-    <img className="is-rounded" src="https://i.imgur.com/CRNpJil.jpg" alt="Image"/>
-          </figure>
-        </div>
-        <div className="media-content">
-          <div className="content">
-            <p>
-              <strong>Dragan Ceran</strong> <small>dragandceran@gmail.com</small>
-              <br />
-              All of our technology is completely unnecessary to a happy life.
-              <br />
-
-
-            </p>
-          </div>
-
-        </div>
-      </article>
-    </div>
-    </div>
-    </div>
-
-
-  </div>
   )
 }
 
